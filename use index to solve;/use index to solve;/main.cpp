a6690121c318260e34215b7285d8730d289e00a3
gouerrrr <90602843+gouerrrr@users.noreply.github.com>
2022-10-28 23:40:07 +0800

    Add files via upload

---
+++ use index to solve;/use index to solve;/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  use index to solve;
+//
+//  Created by 余歌 on 2022/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
